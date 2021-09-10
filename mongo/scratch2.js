ee53daf396f9e649b3c26e502db7bf49c758cfbc
odonfrancisco <francisco.odon11@gmail.com>
2021-09-10 13:28:49 -0400

    more mongo practice. interview starts in 2 min

--- mongo/scratch2.js
+++ mongo/scratch2.js
@@ -10,28 +10,24 @@ const main = () => {
     return new Promise((resolve, reject) => {
         client.connect().then(client => {
             collection = client.db(dbName).collection(collectionName);
+            let cursor;
 
             // const cursor = collection.listIndexes()
             // cursor.toArray().then(indexes => {
             //     resolve(indexes);
             // })
             
-            const query = {
-                wins: {
-                    // $ne: 5,
-                    // $nin: [5,4,3,2,1]
-                    $ne: 2,
-                    // $and: {
-                    //     $ne: 5
-                    // }
-                    // $ne: 1
-                }
-            }
+            // const query = {
+            //     wins: 4,
+                // losses: {
+                //     $in: [3, 2, 5]
+                // }
+            // }
 
-            const cursor = collection.find(query).sort({wins: -1, losses: -1})
-            cursor.toArray().then(documentArr => {
-                console.log("first lmnt");
-                console.log(documentArr[0]);
+            // const cursor = collection.find(query).sort({year: 1})
+            // cursor.toArray().then(documentArr => {
+                // console.log("first lmnt");
+                // console.log(documentArr[0]);
                 // const id = documentArr[0]._id;
                 // collection.updateOne(
                 //     {
@@ -45,8 +41,118 @@ const main = () => {
                 // ).then(newResult => {
                 //     resolve(newResult)
                 // })
-                resolve(documentArr);
-            }).catch(err => {
+            //     resolve(documentArr);
+            // }).catch(err => {
+            //     reject(err);
+            // })
+            // cursor = collection.distinct("laureates.motivation");
+
+            // cursor = collection.find({
+            //     $expr: {
+            //         $function: {
+            //             body: function(name) {return name.includes("contrib")},
+            //             args: ["$laureates.motivation"],
+            //             lang: "js"
+            //         }
+            //     }
+            // })
+
+            // collection.createIndex(
+            //     {
+            //         "laureates.motivation": "text",
+            //         "laureates.firstname": "text",
+            //         "laureates.surname": "text"
+            //     },
+            //     {
+            //         name: "Motivation Text"
+            //     },
+            //     (err, newIndex) => { 
+            //         if(err) reject(err);
+            //         resolve(newIndex);
+            //     }
+            // )
+
+            // const match = {
+            //     $match: {
+            //         $text: {
+            //             $search: "developing"
+            //         }    
+            //     }
+            // }
+
+            // const unwind = {
+            //     $unwind: {
+            //         path: "$laureates"
+            //     }
+            // }
+
+            // const group = {
+            //     $group: {
+            //         _id: "$category",
+            //         laure: {
+            //             "laureates": 1
+            //         }
+            //     }
+            // }
+
+            // cursor = collection.aggregate([match, group, unwind])
+            
+
+            // cursor = collection.distinct("category", {
+            //     "laureates.1": {$exists: true}
+            // });
+            // resolve(cursor);
+
+            // cursor = collection.find({
+            //     // "laureates.share": '1',
+            //     laureates: {
+            //         $size: 1
+            //     },
+
+            //     year: {
+            //         $gte: "2017"
+            //     }
+            //     // $text: {
+            //     //     $search: "contributions"
+            //     // }
+            //     // "laureates.surname": 'Thaler'
+            // }).count()
+            // resolve(cursor);
+            const query = {
+                $text: {
+                    $search: "contribution"
+                },
+                "laureates.motivation": {
+                    $not: { $regex: "^\"for" }
+                } 
+            }
+
+            const unwind = {
+                path: "$laureates"
+            }
+
+            // cursor = collection.aggregate([
+            //     {
+            //         $match: query
+            //     },
+            //     // {
+            //     //     $unwind: unwind
+            //     // }
+            // ])
+            cursor = collection.aggregate([
+                {
+                    $match: query
+                },
+                {
+                    $unwind: unwind
+                }
+            ])
+
+
+            // resolve(cursor);
+            cursor.toArray().then(arr => {
+                resolve(arr);
+            }).then(err => {
                 reject(err);
             })
         })
@@ -55,7 +161,7 @@ const main = () => {
 
 main().then(result => {
     console.log("Finished execution");
-    console.log(result.length);
+    console.log(result);
 }).catch(err => {
     console.error(err);
 })
